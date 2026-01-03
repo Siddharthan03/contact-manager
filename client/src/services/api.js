15ed9fce6fe72fc652b6a6782a56370d1da817a9
Siddharthan R <138519457+Siddharthan03@users.noreply.github.com>
2026-01-03 12:43:08 +0530

    Update API base URL to production server

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://contact-manager-ss65.onrender.com",
 });
 
 export default api;
